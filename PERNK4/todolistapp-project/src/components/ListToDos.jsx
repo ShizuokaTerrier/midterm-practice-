44da17aa06059dd9dc2aef085d542d4367295e53
ShizuokaTerrier <matt@squareoneJapan.com>
2024-02-06 20:40:32 +0900

    CREATE: List to dos

--- PERNK4/todolistapp-project/src/components/ListToDos.jsx
+++ PERNK4/todolistapp-project/src/components/ListToDos.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const ListToDos = ()=>{
 
+const [todos, setTodos]= useState([])
+
+const getTodos = async ()=>{
+  try {
+
+    const response = await fetch("http://localhost:8050/alldata")
+    const jsonData = await response.json()
+
+    setTodos(jsonData)
+
+  } catch (error) {
+    console.error(error.message)
+  }
+}
+
+useEffect(()=>{
+  getTodos()
+},[])
 
     return (
         <>
@@ -14,11 +32,13 @@ const ListToDos = ()=>{
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
+      {todos.map(todo =>(
+        <tr key = {todo.id}>
+          <td>{todo.description}</td>
+          <td>edit</td>
+          <td>delete</td>
+        </tr>
+      ))}
       
     </tbody>
   </table>
