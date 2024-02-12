a3a138f53f7a68d32ba419af4fa9b0d94c8ccad6
ShizuokaTerrier <matt@squareoneJapan.com>
2024-02-12 12:28:44 +0900

    edit to config

--- PERNK4/server/knex.js
+++ PERNK4/server/knex.js
@@ -2,7 +2,7 @@ const knex = require("knex");
 const config = require("./knexfile");
 
 if(process.env.NODE_ENV === 'production'){
-    module.exports = knex(knexConfig.production);
+    module.exports = knex(config.production);
 } else {
-    module.exports = knex(knexConfig.development);
+    module.exports = knex(config.development);
 }
